ffb530ebcfe079713d1f7f898f7ac7c09f8f8e86
Marion Nabulobi <arinabulobi@gmail.com>
2024-10-04 20:00:09 +0300

    add functions continued

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// code your solution here
+function saturdayFun(activity='roller-skate'){
+    return 'This Saturday, I want to roller-skate!';
+}
+console.log(saturdayFun());
+
+function saturdayFun(activity='override'){
+    return "This Saturday, I want to bathe my dog!";
+}
+console.log(saturdayFun("bathe my dog"));
+
+function mondayWork(activity='go to the office'){
+    return "This Monday, I will go to the office.";
+}
+console.log(mondayWork());
+
+function mondayWork(activity="override"){
+    return "This Monday, I will work from home.";
+}
+console.log(mondayWork("work from home"));
+
+function wrapAdjective(result='*'){
+        return "You are *a hard worker*!"
+}
+console.log(wrapAdjective());
+
+
+function wrapAdjective(wrapper='||'){
+        return "You are ||a dedicated programmer||!";
+}
+console.log(wrapAdjective("You are ||a dedicated programmer||!"));
